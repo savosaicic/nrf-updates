6fc5d735718e4ff63e5b8764c42d4a3e9774550f
Savo Saicic <ssaicic@iliad-free.fr>
2026-03-30 14:36:50 +0200

    feat(mcuboot_swap_ext_flash): add dual slot app with secondary slot on external flash

---
+++ apps/mcuboot_swap_ext_flash/src/main.c
@@ -0,0 +1,32 @@
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+
+#define SLEEP_TIME_MS 1000
+
+#define LED0_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+int main(void)
+{
+  int ret;
+
+  if (!gpio_is_ready_dt(&led)) {
+    return 0;
+  }
+
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0) {
+    return 0;
+  }
+
+  while (1) {
+    ret = gpio_pin_toggle_dt(&led);
+    if (ret < 0) {
+      return 0;
+    }
+
+    k_msleep(SLEEP_TIME_MS);
+  }
+  return 0;
+}
